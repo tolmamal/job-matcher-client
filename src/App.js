d4146169026855aaaf6a0a026a7c3fe5506c3f05
tolmamal <tolmamal@gmail.com>
2019-03-18 09:33:29 +0200

    user menu added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
-import SideMenu from "./components/SideMenu";
+import SideMenu from "./components/SideMenu/SideMenu";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 
 function Index() {
     return <h2>Home</h2>;
+        
 }
 
-function About() {
+function About () {
     return <h2>About</h2>;
 }
+
 
 function Users() {
     return <h2>Users</h2>;
@@ -22,12 +24,14 @@ function AppRouter() {
     return (
         <Router>
             <div className="app-container">
-                {/* <SideMenu/> */}
+                <SideMenu/>
                 <Switch>
                     <Route path="/" exact component={Index}/>
+                    {/* <Route path="/menu/" component={SideMenu}/> */}
                     <Route path="/about/" component={About}/>
                     <Route path="/users/" component={Users}/>
                     <Route path="/register/" component={RegisterForm}/>
+                    {/* <Route path="/user/:id/" component={UserPage}/> */}
                 </Switch>
             </div>
         </Router>
--- src/components/SideMenuDemo/SideMenu.js
+++ src/components/SideMenuDemo/SideMenu.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import BurgerMenu from 'react-burger-menu';
-
 import classNames from 'classnames';
 import './SideMenu.css';
-import {Link} from 'react-router-dom';
 
 class MenuWrap extends Component {
   constructor (props) {
@@ -65,46 +63,37 @@ class Demo extends Component {
   getItems() {
     let items;
 
-    items = [
-      <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
-      <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
-      <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
-      <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
-      <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
-      <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
-    ];
-
-    // switch (this.props.menus[this.state.currentMenu].items) {
-    //   case 1:
-    //     items = [
-    //       <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
-    //       <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
-    //       <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
-    //       <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
-    //       <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
-    //       <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
-    //     ];
-    //     break;
-    //   case 2:
-    //     items = [
-    //       <h2 key="0"><i className="fa fa-fw fa-inbox fa-2x" /><span>Sidebar</span></h2>,
-    //       <a key="1" href=""><i className="fa fa-fw fa-database" /><span>Data Management</span></a>,
-    //       <a key="2" href=""><i className="fa fa-fw fa-map-marker" /><span>Location</span></a>,
-    //       <a key="3" href=""><i className="fa fa-fw fa-mortar-board" /><span>Study</span></a>,
-    //       <a key="4" href=""><i className="fa fa-fw fa-picture-o" /><span>Collections</span></a>,
-    //       <a key="5" href=""><i className="fa fa-fw fa-money" /><span>Credits</span></a>
-    //     ];
-    //     break;
-    //   default:
-    //     items = [
-    //       <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
-    //       <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
-    //       <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
-    //       <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
-    //       <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
-    //       <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
-    //     ];
-    // }
+    switch (this.props.menus[this.state.currentMenu].items) {
+      case 1:
+        items = [
+          <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
+          <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
+          <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
+          <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
+          <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
+          <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
+        ];
+        break;
+      case 2:
+        items = [
+          <h2 key="0"><i className="fa fa-fw fa-inbox fa-2x" /><span>Sidebar</span></h2>,
+          <a key="1" href=""><i className="fa fa-fw fa-database" /><span>Data Management</span></a>,
+          <a key="2" href=""><i className="fa fa-fw fa-map-marker" /><span>Location</span></a>,
+          <a key="3" href=""><i className="fa fa-fw fa-mortar-board" /><span>Study</span></a>,
+          <a key="4" href=""><i className="fa fa-fw fa-picture-o" /><span>Collections</span></a>,
+          <a key="5" href=""><i className="fa fa-fw fa-money" /><span>Credits</span></a>
+        ];
+        break;
+      default:
+        items = [
+          <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>,
+          <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>,
+          <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>,
+          <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>,
+          <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>,
+          <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
+        ];
+    }
 
     return items;
   }
@@ -177,18 +166,10 @@ const menus = {
   reveal: {buttonText: 'Reveal', items: 1}
 };
 
-
-class SideMenu extends Component {
-  render() {
-    return (
-        <BurgerMenu.slide>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/users'>Users</Link>
-          <Link to='/register'>Register</Link>
-        </BurgerMenu.slide>
-    );
-  }
+const test = () => {
+  return (
+    <Demo menus={menus} />
+  );
 }
 
-export default SideMenu;
+export default test;
