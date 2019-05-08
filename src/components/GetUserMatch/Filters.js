17ef1f197b06558d88598f579b804a55a50250d7
HannaYair <chenyair1617@gmail.com>
2019-05-08 10:19:21 +0300

    finish thr filters and sorts

--- src/components/GetUserMatch/Filters.js
+++ src/components/GetUserMatch/Filters.js
@@ -38,6 +38,7 @@ class Filters extends Component {
             case 3:{this.StudentJob(); break;}
             case 4:{this.SendingJob();break;}
             case 5:{this.FavoriteJob();break;}
+            case 6:{this.ReplyJob();break;}
             default:{break;}
         }
 
@@ -89,8 +90,9 @@ class Filters extends Component {
             var elem1=document.createElement('a');
             elem1.className='send';
             var send=document.createElement('i');
-            if(this.state.jobs[key][5]==true)
+            if(this.state.jobs[key][5]==true) {
                 send.className = "fa fa-paper-plane";
+            }
             else {
                 send.className = "fa fa-paper-plane";
                 send.classList.toggle('fa-paper-plane-o');
@@ -99,6 +101,20 @@ class Filters extends Component {
             send.addEventListener('click', (event) =>this.UpdateSending(event), false);
             elem1.appendChild(send);
 
+            //replay element
+            var elem2=document.createElement('a');
+            elem2.className='replay';
+            var replay=document.createElement('i');
+            if(this.state.jobs[key][6]==true)
+                replay.className = "fa fa-check-square-o";
+            else {
+                replay.className = "fa fa-square-o";
+                replay.classList.toggle('fa-check-square-o');
+            }
+            replay.id=key+"replay";
+            replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+            elem2.appendChild(replay);
+
             flipCardFront.appendChild(pr);
             flipCardBack.appendChild(ps);
             flipCardBack.appendChild(ll);
@@ -106,6 +122,7 @@ class Filters extends Component {
             flipCardBack.appendChild(brl);
             flipCardBack.appendChild(elem);
             flipCardBack.appendChild(elem1);
+            flipCardBack.appendChild(elem2);
             flipCardInner.appendChild(flipCardFront);
             flipCardInner.appendChild(flipCardBack);
             flipCard.appendChild(flipCardInner);
@@ -185,6 +202,21 @@ class Filters extends Component {
                 send.id=key+"send";
                 send.addEventListener('click', (event) =>this.UpdateSending(event), false);
                 elem1.appendChild(send);
+
+                //replay element
+                var elem2=document.createElement('a');
+                elem2.className='replay';
+                var replay=document.createElement('i');
+                if(this.state.jobs[key][6]==true)
+                    replay.className = "fa fa-check-square-o";
+                else {
+                    replay.className = "fa fa-square-o";
+                    replay.classList.toggle('fa-check-square-o');
+                }
+                replay.id=key+"replay";
+                replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+                elem2.appendChild(replay);
+
                 flipCardFront.appendChild(pr);
                 flipCardBack.appendChild(ps);
                 flipCardBack.appendChild(ll);
@@ -192,6 +224,7 @@ class Filters extends Component {
                 flipCardBack.appendChild(brl);
                 flipCardBack.appendChild(elem);
                 flipCardBack.appendChild(elem1);
+                flipCardBack.appendChild(elem2);
                 flipCardInner.appendChild(flipCardFront);
                 flipCardInner.appendChild(flipCardBack);
                 flipCard.appendChild(flipCardInner);
@@ -269,6 +302,21 @@ class Filters extends Component {
                 send.id=key+"send";
                 send.addEventListener('click', (event) =>this.UpdateSending(event), false);
                 elem1.appendChild(send);
+
+                //replay element
+                var elem2=document.createElement('a');
+                elem2.className='replay';
+                var replay=document.createElement('i');
+                if(this.state.jobs[key][6]==true)
+                    replay.className = "fa fa-check-square-o";
+                else {
+                    replay.className = "fa fa-square-o";
+                    replay.classList.toggle('fa-check-square-o');
+                }
+                replay.id=key+"replay";
+                replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+                elem2.appendChild(replay);
+
                 flipCardFront.appendChild(pr);
                 flipCardBack.appendChild(ps);
                 flipCardBack.appendChild(ll);
@@ -276,6 +324,7 @@ class Filters extends Component {
                 flipCardBack.appendChild(brl);
                 flipCardBack.appendChild(elem);
                 flipCardBack.appendChild(elem1);
+                flipCardBack.appendChild(elem2);
                 flipCardInner.appendChild(flipCardFront);
                 flipCardInner.appendChild(flipCardBack);
                 flipCard.appendChild(flipCardInner);
@@ -351,6 +400,21 @@ class Filters extends Component {
                 send.id=key+"send";
                 send.addEventListener('click', (event) =>this.UpdateSending(event), false);
                 elem1.appendChild(send);
+
+                //replay element
+                var elem2=document.createElement('a');
+                elem2.className='replay';
+                var replay=document.createElement('i');
+                if(this.state.jobs[key][6]==true)
+                    replay.className = "fa fa-check-square-o";
+                else {
+                    replay.className = "fa fa-square-o";
+                    replay.classList.toggle('fa-check-square-o');
+                }
+                replay.id=key+"replay";
+                replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+                elem2.appendChild(replay);
+
                 flipCardFront.appendChild(pr);
                 flipCardBack.appendChild(ps);
                 flipCardBack.appendChild(ll);
@@ -358,6 +422,7 @@ class Filters extends Component {
                 flipCardBack.appendChild(brl);
                 flipCardBack.appendChild(elem);
                 flipCardBack.appendChild(elem1);
+                flipCardBack.appendChild(elem2);
                 flipCardInner.appendChild(flipCardFront);
                 flipCardInner.appendChild(flipCardBack);
                 flipCard.appendChild(flipCardInner);
@@ -435,13 +500,29 @@ class Filters extends Component {
                 send.id=key+"send";
                 send.addEventListener('click', (event) =>this.UpdateSending(event), false);
                 elem1.appendChild(send);
+
+                //replay element
+                var elem2=document.createElement('a');
+                elem2.className='replay';
+                var replay=document.createElement('i');
+                if(this.state.jobs[key][6]==true)
+                    replay.className = "fa fa-check-square-o";
+                else {
+                    replay.className = "fa fa-square-o";
+                    replay.classList.toggle('fa-check-square-o');
+                }
+                replay.id=key+"replay";
+                replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+                elem2.appendChild(replay);
+
                 flipCardFront.appendChild(pr);
                 flipCardBack.appendChild(ps);
                 flipCardBack.appendChild(ll);
                 var brl = document.createElement("br");
                 flipCardBack.appendChild(brl);
                 flipCardBack.appendChild(elem);
-                // flipCardBack.appendChild(iconHeart);
+                flipCardBack.appendChild(elem1);
+                flipCardBack.appendChild(elem2);
                 flipCardInner.appendChild(flipCardFront);
                 flipCardInner.appendChild(flipCardBack);
                 flipCard.appendChild(flipCardInner);
@@ -519,6 +600,21 @@ class Filters extends Component {
                 send.id=key+"send";
                 send.addEventListener('click', (event) =>this.UpdateSending(event), false);
                 elem1.appendChild(send);
+
+                //replay element
+                var elem2=document.createElement('a');
+                elem2.className='replay';
+                var replay=document.createElement('i');
+                if(this.state.jobs[key][6]==true)
+                    replay.className = "fa fa-check-square-o";
+                else {
+                    replay.className = "fa fa-square-o";
+                    replay.classList.toggle('fa-check-square-o');
+                }
+                replay.id=key+"replay";
+                replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+                elem2.appendChild(replay);
+
                 flipCardFront.appendChild(pr);
                 flipCardBack.appendChild(ps);
                 flipCardBack.appendChild(ll);
@@ -526,6 +622,107 @@ class Filters extends Component {
                 flipCardBack.appendChild(brl);
                 flipCardBack.appendChild(elem);
                 flipCardBack.appendChild(elem1);
+                flipCardBack.appendChild(elem2);
+                flipCardInner.appendChild(flipCardFront);
+                flipCardInner.appendChild(flipCardBack);
+                flipCard.appendChild(flipCardInner);
+                column.appendChild(flipCard);
+                if (index < 4)
+                {
+                    row.appendChild(column);
+                    index++;
+                }
+                else {
+                    div.appendChild(row);
+                    row = document.createElement("div");
+                    row.className = 'row';
+                    row.appendChild(column);
+                    index =1;
+                }
+            }
+        }
+        if(row.children.length!=0)
+            div.appendChild(row);
+    };
+
+    ReplyJob = () => {
+        var div = document.getElementById('aaa');
+        var row = document.createElement("div");
+        row.className = 'row';
+        var index = 0;
+        for (var key in this.state.jobs) {
+            if (this.state.jobs[key][6] == true) {
+                var column = document.createElement("div");
+                column.className = 'column';
+                var flipCard = document.createElement("div");
+                flipCard.className = 'flip-card';
+                // flipCard.setAttribute("id", "flip-card");
+                var flipCardInner = document.createElement("div");
+                flipCardInner.className = 'flip-card-inner';
+                var flipCardFront = document.createElement("div");
+                flipCardFront.className = 'flip-card-front';
+                var pr = document.createElement("p");
+                var role = document.createTextNode(this.state.jobs[key][0]);
+                pr.appendChild(role);
+                var flipCardBack = document.createElement("div");
+                flipCardBack.className = 'flip-card-back';
+                var ll = document.createElement("a");
+                var link = document.createTextNode(this.state.jobs[key][1]);
+                ll.appendChild(link);
+                ll.href = this.state.jobs[key][1];
+                var ps = document.createElement("p");
+                var score = document.createTextNode("Score: " + this.state.jobs[key][2].toFixed(2) * 100 + "%");
+                ps.appendChild(score);
+                //heart element
+                var elem=document.createElement('a');
+                elem.className='heart';
+                var heart=document.createElement('i');
+                if(this.state.jobs[key][4]==true)
+                    heart.className = "fa fa-heart";
+                else {
+                    heart.className = "fa fa-heart";
+                    heart.classList.toggle('fa-heart-o');
+                }
+                heart.id=key;
+                heart.addEventListener('click', (event) =>this.UpdateFavorite(event), false);
+                elem.appendChild(heart);
+
+                //send element
+                var elem1=document.createElement('a');
+                elem1.className='send';
+                var send=document.createElement('i');
+                if(this.state.jobs[key][5]==true)
+                    send.className = "fa fa-paper-plane";
+                else {
+                    send.className = "fa fa-paper-plane";
+                    send.classList.toggle('fa-paper-plane-o');
+                }
+                send.id=key+"send";
+                send.addEventListener('click', (event) =>this.UpdateSending(event), false);
+                elem1.appendChild(send);
+
+                //replay element
+                var elem2=document.createElement('a');
+                elem2.className='replay';
+                var replay=document.createElement('i');
+                if(this.state.jobs[key][6]==true)
+                    replay.className = "fa fa-check-square-o";
+                else {
+                    replay.className = "fa fa-square-o";
+                    replay.classList.toggle('fa-check-square-o');
+                }
+                replay.id=key+"replay";
+                replay.addEventListener('click', (event) =>this.UpdateReplay(event), false);
+                elem2.appendChild(replay);
+
+                flipCardFront.appendChild(pr);
+                flipCardBack.appendChild(ps);
+                flipCardBack.appendChild(ll);
+                var brl = document.createElement("br");
+                flipCardBack.appendChild(brl);
+                flipCardBack.appendChild(elem);
+                flipCardBack.appendChild(elem1);
+                flipCardBack.appendChild(elem2);
                 flipCardInner.appendChild(flipCardFront);
                 flipCardInner.appendChild(flipCardBack);
                 flipCard.appendChild(flipCardInner);
@@ -564,6 +761,15 @@ class Filters extends Component {
         const response = await axiosInstance.post(`/user/${this.getUserId()}/UpdateSending`,{body});
     };
 
+    UpdateReplay= async(event)=>{
+        //TODO:to add the perfix fot the it attribute
+        var send = document.getElementById(event.target.id);
+        console.log(event.target.id.substring(0,event.target.id.length-6));
+        send.classList.toggle('fa-square-o');
+        const body={id:event.target.id.substring(0, event.target.id.length-6)};
+        const response = await axiosInstance.post(`/user/${this.getUserId()}/UpdateReply`,{body});
+    };
+
     render() {
         return (
             <div className="filter">
@@ -575,7 +781,7 @@ class Filters extends Component {
                     <button className="btn_nothing" onClick={(event) =>this.filterSelection(3)}> StudentJob</button>
                     <button className="btn_nothing" onClick={(event) =>this.filterSelection(4)}> SendingJob</button>
                     <button className="btn_nothing" onClick={(event) =>this.filterSelection(5)}> FavoriteJob</button>
-
+                    <button className="btn_nothing" onClick={(event) =>this.filterSelection(6)}> ReplyJob</button>
                 </div>
             </div>
         );
