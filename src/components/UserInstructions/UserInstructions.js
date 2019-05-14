6ca501c0c11c6c0fc7ae56213a3f60ae3e553798
tolmamal <tolmamal@gmail.com>
2019-05-14 09:46:47 +0300

    pull

--- src/components/UserInstructions/UserInstructions.js
+++ src/components/UserInstructions/UserInstructions.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./UserInstructions.css"
 import Header from "../Header/Header";
 import Footer from "../Instructions/Fotter";
@@ -6,30 +6,42 @@ import UserModal from "./UserModal";
 
 class UserInstructions extends Component {
 
-    ssssshowClick=(e)=>{
-        var video=document.getElementsByClassName("UIdiv")[0];
-        video.style.display="block";
-
-    };
 
     render() {
         return (
             <div>
-                <Header/>
-                <div>
-                    <h2>Instructions:</h2>
-                    {/*<button onClick={(e)=>this.ssssshowClick(e)}>Show</button>*/}
-                    <div className="UIdiv">
-                        {/*<a href="https://www.youtube.com/watch?v=lm3REFytTHQ" rel="external" target="_blank">jhgfs*/}
-                        {/*</a>*/}
-                        {/*<video>*/}
-                        {/*    <source src={"https://youtu.be/2nO48zhNTYo"}/>*/}
-                        {/*</video>*/}
+                <div className="instructions-page">
+                    <br/>
+                    <br/>
+                    <h1>Instructions</h1>
+                    <br/>
+                    <br/>
+                    <p>
+                        These instructions are designed to show the functionality of the services we offer in our
+                        website.<br/>
+                        We hope that it will be very helpful for you.
+                    </p>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <iframe width="600" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY">
 
-                    </div>
-                    <UserModal/>
+                    </iframe>
+
+
+
                 </div>
-                <Footer/>
+                <br/>
+                <br/>
+                <div className="help-section">
+                    <p>
+                        For any other help, please contact us: &nbsp;
+                        <i className="fa fa-envelope-open" aria-hidden="true"></i> &nbsp; jobmatcher100@gmail.com<br/>
+                        All rights reserved. &nbsp;<i className="fa fa-copyright" aria-hidden="true"></i>
+                    </p>
+
+                </div>
+
             </div>
         );
     }
